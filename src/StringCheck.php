81867ffee79888e43c9f134ef9cc2f1ba16c4a37
Muz4k Victory <muz4k.oo@gmail.com>
2021-12-03 23:39:00 +0300

    added contains validation

--- src/StringCheck.php
+++ src/StringCheck.php
@@ -4,4 +4,42 @@ namespace Hexlet\Validator;
 
 class StringCheck
 {
+    private bool $isRequire = false;
+    private int $minLength = 0;
+    private string $contains = '';
+
+    public function isValid(?string $string): bool
+    {
+        return $this->checkRequire($string) && $this->checkContains($string);
+    }
+
+    public function required()
+    {
+        $this->isRequire = true;
+    }
+
+    public function contains(string $string)
+    {
+        $this->contains = $string;
+
+        return $this;
+    }
+
+    private function checkRequire(?string $string): bool
+    {
+        if ($this->isRequire) {
+            return !empty($string);
+        }
+
+        return true;
+    }
+
+    private function checkContains(?string $string): bool
+    {
+        if ($this->contains) {
+            return str_contains($string, $this->contains);
+        }
+
+        return true;
+    }
 }
